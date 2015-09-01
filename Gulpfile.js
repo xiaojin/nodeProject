648c29cfd8b53639aeaaf1797f2b5c159fc1d4d8
Martin Xiao <martin.xiao@pactera.com.au>
2015-09-01 15:46:29 +1000

    Add express framework

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,7 +1,18 @@
 var gulp = require('gulp'),
   jade = require('gulp-jade'),
   browserify = require('gulp-browserify'),
-  concat = require('gulp-concat');
+  concat = require('gulp-concat'),
+  express = require('express');
+
+
+var server = express();
+
+server.use(express.static('./dist'));
+
+gulp.task('dev', function() {
+  server.listen(8080);
+});
+
 
 gulp.task('js', function() {
   gulp.src(['./app/js/main.js'])
